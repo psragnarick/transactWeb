47ce4a94a5cd2d0cef094d04ca5e1da54bfe7a1b
psragnarick <69256915+psragnarick@users.noreply.github.com>
2021-08-19 13:44:12 -0500

    Update app.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,6 +1,63 @@
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import MaterialButtonTransparentHamburger from "../components/MaterialButtonTransparentHamburger";
+import MaterialButtonPrimary from "../components/MaterialButtonPrimary";
+import MaterialCheckboxWithLabel from "../components/MaterialCheckboxWithLabel";
+import MaterialSearchBar from "../components/MaterialSearchBar";
+import MaterialCardWithoutImage from "../components/MaterialCardWithoutImage";
+
+function Home(props) {
+  return (
+    <>
+      <MaterialButtonTransparentHamburger
+        style={{
+          height: 61,
+          width: 71,
+          marginTop: 52,
+          marginLeft: 286
+        }}
+      ></MaterialButtonTransparentHamburger>
+      <MaterialButtonPrimary
+        style={{
+          height: 36,
+          width: 198,
+          marginTop: 22,
+          marginLeft: 88
+        }}
+      ></MaterialButtonPrimary>
+      <MaterialCheckboxWithLabel
+        style={{
+          height: 40,
+          width: 201,
+          marginTop: 17,
+          alignSelf: "center"
+        }}
+      ></MaterialCheckboxWithLabel>
+      <MaterialSearchBar
+        style={{
+          height: 56,
+          width: 271,
+          marginTop: 2,
+          marginLeft: 50
+        }}
+      ></MaterialSearchBar>
+      <MaterialCardWithoutImage
+        style={{
+          height: 410,
+          width: 271,
+          marginTop: 67,
+          marginLeft: 50
+        }}
+      ></MaterialCardWithoutImage>
+    </>
+  );
+}
+
+export default Home;
+
 'use strict';
 
-// tag::vars[]
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
@@ -181,3 +238,296 @@ render() {
 		</div>
 	)
 }
+
+
+function MaterialSearchBar(props) {
+  return (
+    <Container {...props}>
+      <Rect1>
+        <LeftIconButton>
+          <ButtonOverlay>
+            <MaterialCommunityIconsIcon
+              name="arrow-left"
+              style={{
+                backgroundColor: "transparent",
+                color: "#000",
+                fontSize: 24,
+                opacity: 0.6
+              }}
+            ></MaterialCommunityIconsIcon>
+          </ButtonOverlay>
+        </LeftIconButton>
+        <InputStyleStack>
+          <InputStyle placeholder="Search"></InputStyle>
+          <RightIconButton>
+            <ButtonOverlay>
+              <MaterialCommunityIconsIcon
+                name="close"
+                style={{
+                  backgroundColor: "transparent",
+                  color: "#000",
+                  fontSize: 24,
+                  opacity: 0.6
+                }}
+              ></MaterialCommunityIconsIcon>
+            </ButtonOverlay>
+          </RightIconButton>
+        </InputStyleStack>
+      </Rect1>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  background-color: #3F51B5;
+  padding: 4px;
+  flex-direction: column;
+  box-shadow: 0px 2px 1.2px  0.2px #111 ;
+`;
+
+const ButtonOverlay = styled.button`
+ display: block;
+ background: none;
+ height: 100%;
+ width: 100%;
+ border:none
+ `;
+const Rect1 = styled.div`
+  flex-direction: row;
+  background-color: #FFFFFF;
+  align-items: center;
+  border-radius: 2px;
+  flex: 1 1 0%;
+  margin-bottom: 4px;
+  margin-top: 4px;
+  margin-left: 4px;
+  margin-right: 4px;
+  display: flex;
+`;
+
+const LeftIconButton = styled.div`
+  padding: 11px;
+  flex-direction: column;
+  display: flex;
+  margin-left: 5px;
+  margin-top: 5px;
+  border: none;
+`;
+
+const InputStyle = styled.input`
+  font-family: Roboto;
+  height: 48px;
+  color: #000;
+  padding-right: 5px;
+  font-size: 16px;
+  align-self: flex-start;
+  width: 257px;
+  line-height: 16px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  border: none;
+  background: transparent;
+`;
+
+const RightIconButton = styled.div`
+  padding: 11px;
+  position: absolute;
+  top: 1px;
+  right: 0px;
+  align-items: center;
+  flex-direction: column;
+  display: flex;
+  border: none;
+`;
+
+const InputStyleStack = styled.div`
+  width: 290px;
+  height: 49px;
+  margin-left: 21px;
+  margin-top: 4px;
+  position: relative;
+`;
+
+export default MaterialSearchBar;
+
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+
+function MaterialCardWithoutImage(props) {
+  return (
+    <Container {...props}>
+      <BodyContent></BodyContent>
+      <ActionBody>
+        <ActionButton1>
+          <ButtonOverlay>
+            <Halt>Halt</Halt>
+          </ButtonOverlay>
+        </ActionButton1>
+        <ActionButton2>
+          <ButtonOverlay>
+            <Cancel>Cancel</Cancel>
+          </ButtonOverlay>
+        </ActionButton2>
+      </ActionBody>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  border-width: 1px;
+  border-radius: 2px;
+  border-color: #CCC;
+  flex-wrap: nowrap;
+  background-color: #FFF;
+  overflow: hidden;
+  flex-direction: column;
+  border-style: solid;
+  box-shadow: -2px 2px 1.5px  0.1px #000 ;
+`;
+
+const ButtonOverlay = styled.button`
+ display: block;
+ background: none;
+ height: 100%;
+ width: 100%;
+ border:none
+ `;
+const BodyContent = styled.div`
+  padding: 16px;
+  padding-top: 24px;
+  justify-content: center;
+`;
+
+const ActionBody = styled.div`
+  padding: 8px;
+  flex-direction: row;
+  left: 1px;
+  width: 357px;
+  top: 267px;
+  height: 52px;
+  display: flex;
+`;
+
+const ActionButton1 = styled.div`
+  padding: 8px;
+  height: 36px;
+  flex-direction: column;
+  display: flex;
+  border: none;
+`;
+
+const Halt = styled.span`
+  font-family: Arial;
+  font-size: 14px;
+  color: #000;
+  opacity: 0.9;
+`;
+
+const ActionButton2 = styled.div`
+  padding: 8px;
+  height: 36px;
+  flex-direction: column;
+  display: flex;
+  border: none;
+`;
+
+const Cancel = styled.span`
+  font-family: Arial;
+  font-size: 14px;
+  color: #000;
+  opacity: 0.9;
+`;
+
+export default MaterialCardWithoutImage;
+
+
+function MaterialButtonTransparentHamburger(props) {
+  return (
+    <Container {...props}>
+      <MaterialCommunityIconsIcon
+        name="menu"
+        style={{
+          color: "#3F51B5",
+          fontSize: 24
+        }}
+      ></MaterialCommunityIconsIcon>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  background-color: transparent;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  border-radius: 2px;
+`;
+
+export default MaterialButtonTransparentHamburger;
+
+function MaterialButtonPrimary(props) {
+  return (
+    <Container {...props}>
+      <Caption>BUTTON</Caption>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  background-color: #2196F3;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  border-radius: 2px;
+  min-width: 88px;
+  padding-left: 16px;
+  padding-right: 16px;
+  box-shadow: 0px 1px 5px  0.35px #000 ;
+`;
+
+const Caption = styled.span`
+  font-family: Roboto;
+  color: #fff;
+  font-size: 14px;
+`;
+
+export default MaterialButtonPrimary;
+
+function MaterialCheckboxWithLabel(props) {
+  return (
+    <Container {...props}>
+      <MaterialCommunityIconsIcon
+        name={props.checked ? "checkbox-marked" : "checkbox-blank-outline"}
+        style={{
+          color: "#3F51B5",
+          fontSize: 28,
+          lineHeight: "28px"
+        }}
+      ></MaterialCommunityIconsIcon>
+      <CheckLabel>{props.label || "All previous transactions"}</CheckLabel>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  background-color: transparent;
+  flex-direction: row;
+`;
+
+const CheckLabel = styled.span`
+  font-family: Arial;
+  margin-left: 2px;
+  font-size: 16px;
+  color: rgba(0,0,0,0.87);
+`;
+
+export default MaterialCheckboxWithLabel;
